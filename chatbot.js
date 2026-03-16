5d908fbda6318c0066adb6dea664be1071b7550d
yangsonrachelle-boop <yangsonrachelle@gmail.com>
2026-03-16 21:43:18 +0800

    Update chatbot.js

--- chatbot.js
+++ chatbot.js
@@ -365,7 +365,9 @@ function autoReply(message) {
     // Default fallback
     addBotMessage(currentLang === "tl"
         ? "Paumanhin, ang serbisyong hinahanap ninyo ay hindi available sa klinikang ito."
-        : "Sorry, the service you are looking for is not available in this clinic."
+        : "Sorry, the service you are looking for is not available at this clinic.
+Please type the service exactly as shown in the options so we can provide the correct response.
+"
     );
 }
 function addBotMessage(text) {
